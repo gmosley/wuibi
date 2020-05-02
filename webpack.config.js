f61e5e8319c7d7dd5ffbb06234728e60e0772c47
Graham Mosley <grahammosley123@gmail.com>
2020-05-02 14:56:52 -0700

    update build

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    // for now, copy js/css instead of bundling
+    new CopyPlugin([
+      {
+        from: 'node_modules/webextension-polyfill/dist/browser-polyfill.js',
+        to: 'dist/'
+      },
+      {
+        from: 'node_modules/shaka-player/dist/shaka-player.ui.debug.js',
+        to: 'dist/'
+      },
+      {
+        from: 'node_modules/shaka-player/dist/controls.css',
+        to: 'dist/'
+      }
+    ]),
+  ],
+};
